824ed22acaf0c7de401f560f6546581d70af2187
austin1997 <18709560+austin1997@users.noreply.github.com>
2024-05-29 21:59:10 +0800

    app done

--- app.py
+++ app.py
@@ -4,13 +4,30 @@ from PIL import Image
 import clip
 import json
 import torch
-
+import os
+try:
+    from utils import Utils
+    import qlinear
+except Exception:
+    import setup
+    from utils import Utils
+    import qlinear
 
 class App:
     def __init__(self):
+        model_name = "RN50"
+        onnx_dir = "./onnx/"+model_name
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        self.model, _ = clip.load("ViT-B/32", device=self.device)
+        # self.model, _ = clip.load(model_name, device=self.device)
+        self.model = torch.load(os.path.join(onnx_dir, "text_model_dynamic_quant.pt"))
+        Utils.replace_node( self.model, 
+                    # torch.nn.Linear, 
+                    torch.ao.nn.quantized.dynamic.Linear, 
+                    # qlinear.QLinearPerGrp, 
+                    qlinear.QLinear, 
+                    # (), {'device':'cpu', 'w_bit':4, 'group_size':32} )
+                    (), {'device':'aie', 'kernel_x_shape': (8, 2048), 'kernel_y_shape': (2048, 2048)} )
         self.model.eval()
 
         self.index = read_index("static/index.faiss")
@@ -20,7 +37,7 @@ class App:
     def search(self, search_text, results=1):
         text_tokens = clip.tokenize([search_text]).to(self.device)
         with torch.no_grad():
-            text_features = self.model.encode_text(text_tokens).float()
+            text_features = self.model(text_tokens).float()
         text_features /= text_features.norm(dim=-1, keepdim=True)
         text_features = text_features.cpu().numpy()
 
