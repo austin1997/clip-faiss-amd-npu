e27a16f2f85b628734e210020ae481c6a4b35229
austin1997 <18709560+austin1997@users.noreply.github.com>
2024-05-29 21:45:48 +0800

    index done

--- index.py
+++ index.py
@@ -9,17 +9,58 @@ import json
 import numpy as np
 import os
 import torch
+import onnxruntime
 
+model_name = "RN50"
+onnx_dir = "./onnx/"+model_name
+onnx_path = os.path.join(onnx_dir, "image_model_0_quantized.onnx")
+config_path = 'C:\\Users\\austi\\Downloads\\ryzen-ai-sw-1.1\\ryzen-ai-sw-1.1\\voe-4.0-win_amd64\\vaip_config.json'
+session0 = onnxruntime.InferenceSession(
+            onnx_path,
+            providers=["VitisAIExecutionProvider"],
+            provider_options=[{"config_file":config_path}])
+
+onnx_path = os.path.join(onnx_dir, "image_model_1.onnx")
+sess_options = onnxruntime.SessionOptions()
+
+# Set graph optimization level
+sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
+session1 = onnxruntime.InferenceSession(
+            onnx_path,
+            sess_options=sess_options,
+            providers=["CPUExecutionProvider"])
+
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+def eval(image_input):
+    # Load inputs and do preprocessing by input_shape
+    # print(image_input.shape)
+    input_name = session0.get_inputs()[0].name
+    input_shape = session0.get_inputs()[0].shape
+    for i in range(0, len(input_shape)):
+        if not isinstance(input_shape[i], int):
+            input_shape[i] = 1
+    image_input = np.reshape(image_input, input_shape)
+    npu_qresult = session0.run([], {input_name: image_input})
+    # print("Quantized: ", npu_qresult)
+
+    input_name = session1.get_inputs()[0].name
+    npu_qresult = session1.run([], {input_name: npu_qresult[0]})
+    # image_features = torch.from_numpy(npu_qresult[0])
+    # print("Quantized: ", npu_qresult)
+    return npu_qresult[0]
 
 def index(image_dir_path):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    model, preprocess = clip.load("ViT-B/32", device=device)
+    model, preprocess = clip.load(model_name, device=device, download_root='D:\\Models\\CLIP\\')
     model.eval()
 
     images = []
     image_paths = []
     img_dir_path = image_dir_path
+    image_features = []
     for animal_name in sorted(os.listdir(img_dir_path)):
         print(animal_name)
         if not os.path.isdir(os.path.join(img_dir_path, animal_name)):
@@ -28,15 +69,18 @@ def index(image_dir_path):
             if not img_file.endswith(".jpg"):
                 continue
             image = Image.open(os.path.join(img_dir_path, animal_name, img_file)).convert("RGB")
-            images.append(preprocess(image))
+            # images.append(preprocess(image))
+            image = to_numpy(preprocess(image))
             image_paths.append(os.path.join(img_dir_path, animal_name, img_file))
-    image_input = torch.tensor(np.stack(images)).to(device)
+            image_features.append(eval(image)[0])
 
-    with torch.no_grad():
-        image_features = model.encode_image(image_input).float()
+    # image_input = torch.tensor(np.stack(images)).to(device)
+    image_features = torch.tensor(np.stack(image_features)).to(device)
+    # with torch.no_grad():
+    #     image_features = model.encode_image(image_input).float()
     image_features /= image_features.norm(dim=-1, keepdim=True)
     image_features = image_features.cpu().numpy()
-
+    print(image_features.shape)
     index = faiss.IndexFlatIP(image_features.shape[1])
     index.add(image_features)
     write_index(index, "static/index.faiss")
@@ -47,6 +91,7 @@ def index(image_dir_path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_dir_path", type=str, default="static/data/images")
+    # parser.add_argument("--image_dir_path", type=str, default="static/data/images")
+    parser.add_argument("--image_dir_path", type=str, default="D:\\Datasets\\ye-pop\\images\\chunk_1")
     args = parser.parse_args()
     index(args.image_dir_path)
